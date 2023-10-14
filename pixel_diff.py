ec45444731b08c6be9d2191561a1a4ae03601665
ketanpuyad2 <84304120+ketanpuyad2@users.noreply.github.com>
2023-10-14 20:45:49 +0530

    Add files via upload
    Pixel Difference and psnr

---
+++ pixel_diff.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+def pixel_difference(original_img,decrypted_img):
+   
+    # Check if the images have the same dimensions
+    if original_img.shape != decrypted_img.shape:
+        raise ValueError("Images must have the same dimensions")
+    normalized_image = cv2.normalize(original_img, None, 0, 255, cv2.NORM_MINMAX)
+    # Compute the absolute pixel-wise difference
+    difference = cv2.absdiff(original_img, decrypted_img)
+    return difference
+
+def display_image_from_array(diff_img,title):
+    print(np.ndarray.sum(diff_img))
+    # Create an image from the 2D array
+    image = cv2.merge([diff_img])
+    img = np.uint8(image)
+
+    # Show the image (optional)
+    cv2.imshow(title, img)
